e47a3656bdd345afd822dae42dc29349348999d0
nizarmonza <nizarmonzaa@gmail.com>
2022-07-19 20:34:16 +0000

    unfinished

--- src/models.py
+++ src/models.py
@@ -6,28 +6,65 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy import render_er
 
+
+
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    first_name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Favorites(Base):
+    __tablename__ = 'favorites'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    user_id = Column(Integer, ForeignKey('User.id'))
+    planets = Column(String, ForeignKey('Planets.id'))
+    characters = Column(String, ForeignKey('Characters.id'))
+    
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256))
+    diameter = Column(Integer)
+    rotation_period = Column(Integer, nullable=False)
+    orbital_period = Column(Integer)
+    gravity = Column(Integer, nullable=False)
+    population = Column(Integer, nullable=False)
+    climate = Column(String(256), nullable=False)
+    terrain = Column(String(256), nullable=False)
+    surface_water = Column(String(256), nullable=False)
+    url = Column(String(256), nullable=False)
+
+class Characters(Base):
+    __tablename__ = 'characters'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(256))
+    name = Column(String(256))
+    birth_year = Column(Integer)
+    eye_color = Column(String(256), nullable=False)
+    gender = Column(String(256), nullable=False)
+    hair_color = Column(String(256), nullable=False)
+    height = Column(Integer, nullable=False)
+    mass = Column(Integer, nullable=False)
+    skin_color = Column(String(256), nullable=False)
+    homeworld = Column(String(256), nullable=False)
+    url = Column(String(256), nullable=False)
 
     def to_dict(self):
         return {}
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
+
+
